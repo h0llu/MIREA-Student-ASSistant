16823524fd7ef1fc74d9606fde94938e47442453
Tuman-42 <zverv.Sasha@yandex.ru>
2021-03-11 21:46:28 +0300

    1commit

--- OURbot.py
+++ OURbot.py
@@ -1,14 +1,16 @@
 import telebot
 import Users # здесь происходят все операции с данными о пользователях
 
-bot = telebot.TeleBot('ТОКЕН')
+bot = telebot.TeleBot('1625713494:AAEafifP5xHlVFC2wb7jcaWpHJHL4H2csWQ')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    Users.create(message.chat.id)
     bot.send_message(message.chat.id, 'Привет, этот бот пока ничего не может, но скоро научиться!')
 
 @bot.message_handler(content_types=['text'])
 def answer_to_text(message):
-    user = Users.check(message.from_user.id)
+    user_locate = Users.check(message.from_user.id)
+    bot.send_message(message.chat.id, str(user_locate))
 
 bot.polling()
--- Users.py
+++ Users.py
@@ -4,9 +4,18 @@ def create(user_id):
     conn = sqlite3.connect("Users.db")
     cursor = conn.cursor()
 
-    cursor.execute("""CREATE TABLE Users
-                  (id INT PRIMARY KEY,
-                   location int)с
-                   """)
+    cursor.execute("SELECT * FROM Users WHERE id = "+str(user_id))
+    
+    if len(cursor.fetchall())==0:
+        cursor.execute('INSERT INTO Users VALUES('+str(user_id)+',0)')
+    conn.commit()
+    conn.close()
 
 def check(user_id):
+    conn = sqlite3.connect("Users.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM Users WHERE id = "+str(user_id)+"")
+    user_info = cursor.fetchall()
+    conn.close()
+    return user_info
