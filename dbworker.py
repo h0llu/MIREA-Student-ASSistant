5b212c58de522739450c762cacdb07902f48f52c
h0llu <g.govzheev001@gmail.com>
2021-03-13 23:38:22 +0300

    Написал БД, которая хранит расписание

--- dbworker.py
+++ dbworker.py
@@ -3,7 +3,7 @@ import sqlite3
 # Хранит ID пользователя и его состояние в дереве диалогов
 class Users:
     __path__ = 'Databases/Users.db'
-    def create_table(self):
+    def __init__(self):
         con = sqlite3.connect(self.__path__)
         cursor = con.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS Users
@@ -25,13 +25,12 @@ class Users:
         cursor.execute(f'SELECT state FROM Users WHERE id = {user_id}')
         con.commit()
         state = cursor.fetchone()[0]
-        print('get state:', state)
         return state
 
 # Хранит ID пользователя и группы, на которые он подписан
 class Users_subs:
     __path__ = 'Databases/Users_subs.db'
-    def create_table(self):
+    def __init__(self):
         con = sqlite3.connect(self.__path__)
         cursor = con.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS Groups
@@ -52,10 +51,46 @@ class Users_subs:
         con.commit()
         return cursor.fetchall()
 
+    def del_group(self, id, group):
+        con = sqlite3.connect(self.__path__)
+        cursor = con.cursor()
+        cursor.execute(f'DELETE FROM Groups WHERE ( id = {id} AND group = {group} )')
+        con.commit()
+
+# Хранит расписание в виде
+# (имя группы, дата, названия предмета, номер пары, аудитория, преподаватель)
+class Schedule:
+    __path__ = 'Databases/Schedule.db'
+    def init(self):
+        con = sqlite3.connect(self.__path__)
+        cursor = con.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS Schedule
+        (group TEXT NOT NULL,
+        date TEXT NOT NULL,
+        title TEXT NOT NULL,
+        order INTEGER NOT NULL,
+        classroom TEXT NOT NULL,
+        teacher TEXT NOT NULL)''')
+        con.commit()
+
+    def set_lesson(self, group, date, title, order, classroom, teacher):
+        con = sqlite3.connect(self.__path__)
+        cursor = con.cursor()
+        cursor.execute('INSERT OR REPLACE INTO Schedule VALUES (?, ?, ?, ?, ?, ?)',
+        [group, date, title, order, classroom, teacher])
+        con.commit()
+
+    def get_lesson(self, group, date):
+        con = sqlite3.connect(self.__path__)
+        cursor = con.cursor()
+        cursor.execute(f'SELECT * FROM Schedule WHERE ( group = {group} AND date = {date} )')
+        con.commit()
+        return cursor.fetchall()
+
 # Хранит имя преподавателя и его описание
 class Professors:
     __path__ = 'Databases/Professors.db'
-    def create_table(self):
+    def __init__(self):
         con = sqlite3.connect(self.__path__)
         cursor = con.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS Professors
@@ -78,10 +113,17 @@ class Professors:
         cursor.execute('INSERT OR REPLACE INTO Professors VALUES (?, ?)', [name, description])
         con.commit()
 
+    def get_description(self, name):
+        con = sqlite3.connect(self.__path__)
+        cursor = con.cursor()
+        cursor.execute(f'SELECT description FROM Professors WHERE name = {name}')
+        con.commit()
+        return cursor.fetchone()[0]
+
 # Хранит скидки (просто еду) из Виктории
 class Victoria:
     __path__ = 'Databases/Victoria.db'
-    def create_table(self):
+    def __init__(self):
         con = sqlite3.connect(self.__path__)
         cursor = con.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS Victoria
@@ -109,7 +151,3 @@ class Victoria:
         cursor.execute('SELECT * FROM Victoria')
         con.commit()
         return cursor.fetchall()
-
-# Хранит расписание всех групп
-class Groups:
-    pass
